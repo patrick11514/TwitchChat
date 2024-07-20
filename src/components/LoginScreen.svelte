0cf75b021d7cb3a2e7c45e74904eb3d3be96159a
Patrik Mintěl <ja@patrick115.eu>
2024-07-20 23:48:07 +0200

    Added redirect

---
+++ src/components/LoginScreen.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { PUBLIC_CLIENT_ID, PUBLIC_REDIRECT_URI } from '$env/static/public';
+    import Button from './Button.svelte';
+    import Title from './Title.svelte';
+
+    const login = () => {
+        const searchParams = new URLSearchParams();
+        searchParams.append('response_type', 'token');
+        searchParams.append('client_id', PUBLIC_CLIENT_ID);
+        searchParams.append('redirect_uri', PUBLIC_REDIRECT_URI);
+
+        window.location.assign(`https://id.twitch.tv/oauth2/authorize?${searchParams.toString()}&scope=${['chat:read', 'chat:edit'].map(encodeURIComponent).join('+')}`);
+    };
+</script>
+
+<div class="bg-secondary m-auto flex flex-col gap-2 rounded-md p-2">
+    <Title>Login</Title>
+    <Button on:click={login}>Click to login</Button>
+</div>
