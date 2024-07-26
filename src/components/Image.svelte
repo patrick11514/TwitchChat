39bff890af3f1d98bc00c76ff9a90feb36ac088b
Patrik Mintěl <ja@patrick115.eu>
2024-07-27 01:14:02 +0200

    Image component with disabled drag

---
+++ src/components/Image.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    export let src: string | null | undefined;
+    export let alt: string;
+    let cls = '';
+    export { cls as class };
+</script>
+
+<img on:dragstart|preventDefault class={cls} {src} {alt} />
