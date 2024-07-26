c55e9da1e5be324c49d118003349ce258013ee22
Patrik Mintěl <ja@patrick115.eu>
2024-07-27 01:14:14 +0200

    Added onkeypress event to forward

--- src/components/Input.svelte
+++ src/components/Input.svelte
@@ -9,4 +9,4 @@
     export { cls as class };
 </script>
 
-<input {id} on:input on:input={(ev) => (value = ev.currentTarget.value)} {type} {value} class={twMerge(' text-t rounded-md bg-primary px-2 py-1 font-medium', cls)} />
+<input on:keypress {id} on:input on:input={(ev) => (value = ev.currentTarget.value)} {type} {value} class={twMerge(' text-t rounded-md bg-primary px-2 py-1 font-medium', cls)} />
