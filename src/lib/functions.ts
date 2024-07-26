b25207bbd17dbfd50f35c72e1b386062fb518eca
Patrik Mintěl <ja@patrick115.eu>
2024-07-26 22:56:37 +0200

    Default assets and custom feetch function

--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -1,5 +1,6 @@
 import { browser } from '$app/environment';
 import Swal, { type SweetAlertOptions } from 'sweetalert2';
+import z from 'zod';
 
 export const SwalAlert = async (data: SweetAlertOptions) => {
     if (!browser) {
@@ -17,4 +18,28 @@ export const SwalAlert = async (data: SweetAlertOptions) => {
         showConfirmButton: false,
         ...data
     });
+};
+
+export const DEFAULT_ASSETS = {
+    TWITCH_DEFAULT_BADGE: 'https://assets.twitch.tv/assets/dark-40f6c299eb07b670b88d.svg'
+} as const;
+
+export const customFetch = async <$ReturnType>(url: string, options: RequestInit, schema: z.ZodType<$ReturnType>): Promise<$ReturnType | z.ZodError<$ReturnType> | null> => {
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+        return null;
+    }
+    try {
+        const json = await response.json();
+        const data = schema.safeParse(json);
+
+        if (!data.success) {
+            return data.error;
+        }
+
+        return data.data;
+    } catch (_) {
+        return null;
+    }
 };
