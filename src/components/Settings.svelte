b00226c0fbf1e221d9316830ab016075a0d663df
Patrik Mintěl <ja@patrick115.eu>
2024-08-21 16:18:27 +0200

    WIP settings

---
+++ src/components/Settings.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { invoke } from '@tauri-apps/api';
+
+    export const logout = async () => {
+        const result = await invoke('logout');
+    };
+</script>
+
+Nastavenííí :))))
